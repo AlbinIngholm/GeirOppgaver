1e8d2d47f08ad9c7c2bcb1fe929092ac1d0091e1
Albin Ingholm <albin.ingholm@gmail.com>
2024-08-26 14:34:17 +0200

    First commit

---
+++ Oppgave01/variabler.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Oppgi navn");
+string? name = Console.ReadLine();
+Console.WriteLine("Hei " + name);
+
+int var1 = 8;
+int var2 = 16;
+Console.WriteLine(var1 + "\n" + var2);
+
+Console.WriteLine("Oppgi et nytt navn");
+string? name2 = Console.ReadLine();
+string name3 = name + " og " + name2;
+Console.Write(name3);
+
+Console.ReadLine();
