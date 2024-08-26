e72a02b81a59b532f1dcf8714861f902d488e480
Albin Ingholm <albin.ingholm@gmail.com>
2024-08-26 14:41:19 +0200

    Oppgave 2

---
+++ Oppgave02/Beslutninger.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Har du lyst på en brus? (ja/nei)");
+string? lystPåBrus = Console.ReadLine();
+
+switch (lystPåBrus)
+{
+    case "ja":
+        Console.Write("Her har du en brus!");
+        break;
+    case "nei":
+        Console.Write("Den er grei.");
+        break;
+    default:
+        Console.WriteLine("Det forstod jeg ikke helt.");
+        break;
+}
