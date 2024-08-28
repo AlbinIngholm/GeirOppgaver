143d48062c11b7171e822e2e138d44b07a442b74
Albin Ingholm <albin.ingholm@gmail.com>
2024-08-28 10:57:53 +0200

    Oppgave 4

---
+++ Oppgave04/Program.cs
@@ -0,0 +1,15 @@
+﻿static void SkrivUt()
+{
+    Console.Write("Skriv inn navn: ");
+    string? name = Console.ReadLine();
+    Console.Write("Hvor kommer du fra?: ");
+    string? origin = Console.ReadLine();
+
+    string message = $"Hei, {name}! Du er fra {origin}";
+    Console.WriteLine(message);
+
+}
+
+SkrivUt();
+SkrivUt();
+SkrivUt();
