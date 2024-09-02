508fc76ae752e8dec914dccc89a4a26ec8c36dae
Albin Ingholm <albin.ingholm@gmail.com>
2024-09-02 14:18:15 +0200

    Skrevet kode for oppgave 6

---
+++ Oppgave06/Program.cs
@@ -0,0 +1,22 @@
+﻿static void microsoftCounter()
+{
+    int microsoftMoment = 0;
+
+    while (true)
+    {
+        Console.WriteLine("Har du nylig hatt en Microsoft Moment? y/n");
+        string? input = Console.ReadLine();
+        if (input == "y")
+        {
+            microsoftMoment++;
+            Console.WriteLine("Tenkte meg det. Antall microsoft moment er nå: " + microsoftMoment);
+        }
+        else
+        {
+            microsoftMoment++;
+            Console.WriteLine("Umulig. Antall microsoft moment er nå: " + microsoftMoment);
+        }
+    }
+}
+
+microsoftCounter();
