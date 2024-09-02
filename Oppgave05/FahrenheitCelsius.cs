c3a618d21e1b02046c4117e72cc122c8ea4e8dda
Albin Ingholm <albin.ingholm@gmail.com>
2024-09-02 12:47:52 +0200

    Oppgave 5

---
+++ Oppgave05/FahrenheitCelsius.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Skriv inn temperaturen du ønsker å konvertere fra F til C: ");
+double input = Convert.ToInt32(Console.ReadLine());
+
+double celsius = FtoC(input);
+Console.WriteLine("Temperatur i Celsius: " + celsius);  
+static double FtoC(double F)
+{
+    Console.Write("\nTemperatur i Fahrenheit: "+F);
+    double C = (F - 32) * 5 / 9;
+    return C;
+}
