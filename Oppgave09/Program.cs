db786e17604bee103c72e9144271eff5753b89b0
Albin Ingholm <albin.ingholm@gmail.com>
2024-09-18 11:30:42 +0200

    Oppgave 9

---
+++ Oppgave09/Program.cs
@@ -0,0 +1,38 @@
+﻿static void billett(int age, string? name) // funksjon som tar inn age og name som parametere
+{
+    // Kode for å hente navn og alder som input, men bruker ikke det når jeg skal teste programmet med forskjellige parametere.
+    
+    /*Console.WriteLine("Skriv inn navnet ditt: ");
+    name = Console.ReadLine();
+
+    Console.WriteLine("Skriv inn alderen din: ");
+    age = Convert.ToInt32(Console.ReadLine());*/
+
+    // initialiserer en billettpris variabel
+    int billettpris = 0;
+
+
+    // sjekker om alder matcher med spesifike kriterer, og setter billettpris til riktig verdi
+    switch (age)
+    {
+        case <= 17:
+            billettpris = 30;
+            break;
+
+        case >= 63:
+            billettpris = 35;
+            break;
+
+        case > 17 and < 63:
+            billettpris = 50;
+            break;
+    }
+
+    // printer ut billettprisen
+    Console.WriteLine($"Hei, {name}. Din billettpris er {billettpris}kr.");
+}
+
+billett(15,"Test");
+billett(31, "Test");
+billett(63, "Test");
+
