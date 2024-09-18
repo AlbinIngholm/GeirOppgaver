2db2224071778f3f0daa9738b0cad1fa0c66184f
Albin Ingholm <albin.ingholm@gmail.com>
2024-09-18 19:15:17 +0200

    Lagt til kommentarer på oppgave 10

--- Oppgave10/Program.cs
+++ Oppgave10/Program.cs
@@ -1,4 +1,5 @@
-﻿Dictionary<string, List<string>> måltider = new Dictionary<string, List<string>>
+﻿// Ordbok med beboere som key, og måltider som value
+Dictionary<string, List<string>> måltider = new Dictionary<string, List<string>>
 {
     {"Kari",new List<string> {"Egg og bacon","Salat","Fisk"} },
     {"Per", new List<string> {"Frokostblanding","Brødskive med gulost","Kylling" } },
@@ -7,23 +8,23 @@
 
 };
 
-skrivUt(måltider);
+skrivUt(måltider); // kaller på funksjonen for skrive ut og hente måltider
 
-static void skrivUt(Dictionary<string, List<string>> måltider)
+static void skrivUt(Dictionary<string, List<string>> måltider) // funksjon som tar inn måltider som parameter
 {
     Console.Write("Beboere:\n");
-    foreach(var person in måltider.Keys)
+    foreach(var person in måltider.Keys) // looper gjennom for hver key i ordboka. 
     {
-        Console.WriteLine(person);
+        Console.WriteLine(person); // skriver ut key
     }
 
     Console.WriteLine("\nSkriv inn navnet på beboeren du ønsker å hente matplan for: ");
-    string? beboerInput = Console.ReadLine();
+    string? beboerInput = Console.ReadLine(); // catcher brukerinput
 
-    if (måltider.ContainsKey(beboerInput))
+    if (måltider.ContainsKey(beboerInput)) // sjekker om ordboka inneholder brukerinput
     {
-        List<string> dagsmåltid = måltider[beboerInput];
-        Console.WriteLine($"\nFrokost: {dagsmåltid[0]}");
+        List<string> dagsmåltid = måltider[beboerInput]; // liste som henter value fra ordboka, med brukerinput som key
+        Console.WriteLine($"\nFrokost: {dagsmåltid[0]}"); // skriver ut index 0 av key
         Console.WriteLine($"Lunsj: {dagsmåltid[1]}");
         Console.WriteLine($"Middag: {dagsmåltid[2]}");
     } else
